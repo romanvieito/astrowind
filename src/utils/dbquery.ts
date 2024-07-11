a66cacdeb46821c58afc33b185b0fa1475cbbb54
adony gonzalez cardenas <adonyglez850209@gmail.com>
2024-07-11 04:39:13 -0400

    adding users waitlist in database v27

--- src/utils/dbquery.ts
+++ src/utils/dbquery.ts
@@ -26,7 +26,7 @@ export async function getUsersWaitListByEmail(email: string) {
 export async function addUsersWaitList(data: TUserWaitList) {
 
   const temp = await getUsersWaitListByEmail(data.email!);
-  if (temp) return [];
+  if (temp) return new Array([]);
   return [{
     id: '0',
     fullname: 'x',
