09fa94eb2319339a90363fd290ed782351cd7c1c
adony gonzalez cardenas <adonyglez850209@gmail.com>
2024-07-17 02:20:30 -0400

    adding mixpanel v5

---
+++ src/components/SendMsgToMixPanel.tsx
@@ -0,0 +1,32 @@
+import { createEffect } from 'solid-js';
+import mixpanel from "mixpanel-browser";
+
+interface SendMsgProps {
+  apitoken?: string;
+  message?: string;
+}
+
+const SendMsgToMixPanel = ({apitoken, message}:SendMsgProps) => {
+  
+  mixpanel.init(apitoken ?? '', {
+    debug: true,
+    track_pageview: false,
+    persistence: "localStorage",
+  });
+
+  createEffect(() => {
+    mixpanel.track(message ?? '', {
+    }, function(err) {
+      if (err) {
+        console.error('Error al enviar evento a mixpanel:', err);
+      } else {
+        console.log('Evento enviado a mixpanel');
+      }
+    });    
+  }, []);
+  return (
+    <></>
+  );
+};
+
+export default SendMsgToMixPanel;
