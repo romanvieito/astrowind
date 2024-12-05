ee8455fb48a0de63aa9ddc1afe022d89bc960c65
Alberto Roman <romanvieito@gmail.com>
2024-12-05 08:58:08 +0100

    md comentado

--- src/pages/api/generateMarkdown.js
+++ src/pages/api/generateMarkdown.js
@@ -1,40 +1,40 @@
-import { Dropbox } from 'dropbox';
-import { sql } from '@vercel/postgres';
+// import { Dropbox } from 'dropbox';
+// import { sql } from '@vercel/postgres';
 
-// Initialize Dropbox with your access token
-const dbx = new Dropbox({ 
-    accessToken: process.env.DROPBOX_ACCESS_TOKEN
-});
+// // Initialize Dropbox with your access token
+// const dbx = new Dropbox({ 
+//     accessToken: process.env.DROPBOX_ACCESS_TOKEN
+// });
 
-export default async function handler(req, res) {
-    try {
-        const result = await sql`
-            SELECT title, content, published_at 
-            FROM blog_posts 
-            ORDER BY published_at ASC;
-        `;
+// export default async function handler(req, res) {
+//     try {
+//         const result = await sql`
+//             SELECT title, content, published_at 
+//             FROM blog_posts 
+//             ORDER BY published_at ASC;
+//         `;
 
-        for (let i = 0; i < result.rows.length; i++) {
-            const post = result.rows[i];
-            const markdownContent = `---
-publishDate: ${post.published_at.toISOString()}
-title: ${post.title}
----
-${post.content}`;
+//         for (let i = 0; i < result.rows.length; i++) {
+//             const post = result.rows[i];
+//             const markdownContent = `---
+// publishDate: ${post.published_at.toISOString()}
+// title: ${post.title}
+// ---
+// ${post.content}`;
 
-            // Upload to Dropbox
-            await dbx.filesUpload({
-                path: `/blog-posts/${i + 1}.md`,
-                contents: markdownContent
-            });
+//             // Upload to Dropbox
+//             await dbx.filesUpload({
+//                 path: `/blog-posts/${i + 1}.md`,
+//                 contents: markdownContent
+//             });
             
-            console.log(`Uploaded post ${i + 1} to Dropbox`);
-        }
+//             console.log(`Uploaded post ${i + 1} to Dropbox`);
+//         }
 
-        console.log('All files uploaded to Dropbox!');
-        res.status(200).send('Markdown files generated and uploaded.');
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Error generating markdown files.');
-    }
-} 
+//         console.log('All files uploaded to Dropbox!');
+//         res.status(200).send('Markdown files generated and uploaded.');
+//     } catch (error) {
+//         console.error('Error:', error);
+//         res.status(500).send('Error generating markdown files.');
+//     }
+// } 
